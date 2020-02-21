7f226ee3466bbf9258d4964c42ba1c904353aba9
whatshappens@gmail.com <whatshappens@gmail.com>
2020-02-21 14:04:53 +0100

    First Commit

---
+++ auth.php
@@ -0,0 +1,157 @@
+<?
+// Copyright 2002 Brian Ronald.  All rights reserved.
+// Portable PHP/MySQL Corporate Intranet System
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+//////////////////////////////////////////////////////////////////////////////
+
+// This file does the password checking and session variable setting
+// required to create a logged in session.
+
+// Session stuff, and general paranoia
+   extract($_REQUEST);
+   if(isset($userid))
+   {
+      unset($userid);
+      unset($username);
+      unset($firstname);
+      unset($lastname);
+   }
+   session_name('PPCIS');
+   session_start();
+   $loginhost=$_SERVER["SERVER_NAME"];
+   $userid = 0; // 0 = not logged in
+if(!(@ include("settings.inc")))
+{
+   header("location:".$_SERVER["HTTP_REFERER"]);
+   die("Database not configured.");
+}
+
+function showerror()
+{
+   if(mysqli_connect_errno())
+   {
+      print("Couldn't connect to database.");
+      if(@mysqli_error($intranet_db))
+         print("Error number " . mysqli_errno($intranet_db) . " (" . mysqli_error($intranet_db) . ")");
+   }
+}
+
+function safe_escape($str) 
+{
+   if (1 == (int) ini_get("magic_quotes_gpc"))
+   {
+      return $str;
+   }
+   else
+   {
+      return addslashes($str);
+   }
+}
+
+// Connect to the database, run a select query. Retrieve any row
+// with matching usernanme, and verify the password. Set the userid
+// and the user is logged in.
+
+if(isset($form_user))
+{
+// Connect to the database
+   $intranet_db = @ mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+   showerror();
+   $sql = "SELECT * FROM users WHERE username = '".safe_escape(trim($form_user))."'";
+   $result = @ mysqli_query($intranet_db, $sql);
+   showerror();
+   if(@ mysqli_num_rows($result) != 0)
+   {
+      while($row = @ mysqli_fetch_array($result,MYSQLI_ASSOC))
+      {
+         if($row["enabled"]=='y' and password_verify($form_pass, $row["password"]))
+         {
+            $userid = $row["userid"];
+            $username = $form_user;
+            $firstname = $row["firstname"];
+            $lastname = $row["lastname"];
+         }
+      }
+   }
+   if($userid == 0 and isset($hash_function))
+   {
+      $sql = "SELECT * FROM users WHERE username = '".safe_escape(trim($form_user))."' AND password = $hash_function( '".safe_escape($form_pass)."')";
+      $result = @ mysqli_query($intranet_db,$sql);
+      showerror();
+      if(@ mysqli_num_rows($result) != 0)
+      {
+         while($row = @ mysqli_fetch_array($result,MYSQLI_ASSOC))
+         {
+            if($row["enabled"]=='y')
+            {
+               $userid = $row["userid"];
+               $username = $form_user;
+               $firstname = $row["firstname"];
+               $lastname = $row["lastname"];
+            }
+            $sql = "UPDATE users SET password = '".password_hash($form_pass,PASSWORD_DEFAULT)."' WHERE userid = ".$row["userid"];
+            $result = @ mysqli_query($intranet_db,$sql);
+            $updates = "Updated password from $hash_function to secure hash with salt.";
+         }
+      }
+   }
+   if ($userid == 0)
+   {
+      $username = "failed";
+      $firstname = "";
+      $lastname = "";
+      session_destroy();
+   }
+}
+else
+{
+   $userid = 0;
+   $username = "logout";
+   $firstname = "";
+   $lastname = "";
+   session_destroy();
+}
+$_SESSION["userid"]=$userid;
+$_SESSION["firstname"]=$firstname;
+$_SESSION["lastname"]=$lastname;
+$_SESSION["loginhost"]=$loginhost;
+$_SESSION["help_keyword"]=$help_keyword;
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<? // This returns to the main system.
+if(isset($logout))
+{
+   print("<META HTTP-EQUIV=\"refresh\" content=\"0;URL=$siteprefix" . "index.php?auth=out\">");
+}
+else
+{
+   print("<META HTTP-EQUIV=\"refresh\" content=\"0;URL=$siteprefix" . "index.php?auth=in\">");
+}
+?>
+<title>Authentication</title>
+</head>
+<body>
+<?
+print("Authorisation returned: userid=$userid, username=$username.<br>\n");
+print("If you aren't automatically returned, click <a href=\"$site_prefix" . "index.php\">here</a><br>\n");
+print($updates);
+?>
+</body>
+</html>
---
+++ password.php
@@ -0,0 +1,106 @@
+<?
+// Copyright 2002 Brian Ronald.  All rights reserved.
+// Portable PHP/MySQL Corporate Intranet System
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+//////////////////////////////////////////////////////////////////////////////
+
+// By default, this is called from the banner, but if the banner is modified
+// this script can be included as a module.
+
+require("head.inc");
+// Guest users cannot change their own password.
+if(isset($mod_userid))
+{
+   $sql = "SELECT * FROM users WHERE userid = $userid";
+   $result = @ mysqli_query($intranet_db, $sql);
+   showerror();
+   if(@ mysqli_num_rows($result) != 0)
+   {
+      while($row = @ mysqli_fetch_array($result,MYSQLI_ASSOC))
+      {
+         if($row["enabled"]=='y' and password_verify($old_password, $row["password"]))
+         {
+            if(($mod_password==$mod_confirm) AND ($mod_password<>"")) // Make sure new password was typed twice, and isn't null
+            {
+               if($mod_password <> "") $mod_password = password_hash($mod_password,PASSWORD_DEFAULT);
+               $sql = "UPDATE users SET password='$mod_password' WHERE userid = $mod_userid";
+               $updateresult = @ mysqli_query($intranet_db,$sql);
+               showerror();
+               if( @ mysqli_affected_rows($intranet_db) != 0)
+               {
+                  print("<span class=\"message\">".$lang['your_password_changed']."</span>");
+               }
+            }
+            else
+            {
+               print("<span class=\"message\">".$lang['password_change_mismatch']."</span>");
+            }
+         }
+         else
+         {
+            print("<span class=\"message\">".$lang['password_change_wrong']."</span>");
+         }
+      }
+   }
+}
+$sql = "SELECT * FROM users WHERE userid = $userid and guest = 'n'";
+$result = @ mysqli_query($intranet_db,$sql);
+showerror();
+if(@ mysqli_num_rows($result) != 0)
+{
+   print("<table>");
+   $row = @ mysqli_fetch_array($result,MYSQLI_ASSOC); // Get user info.  User must confirm old password.
+   print("<form method=\"post\" id=\"usermod\" action=\"$PHP_SELF\">\n");
+   print("<input type=\"hidden\" name=\"mod_userid\" value=\"".$row["userid"]."\">");
+   print("<tr>");
+   print("<th colspan=\"2\"><h2>");
+   print("Modifying user: ");
+   print($row["firstname"] . " " . $row["lastname"] ."</h2></th>");
+   print("</tr>\n");
+   print("<tr>\n");
+   print("<td class=\"right\">".$lang['your_username'].":</td>\n");
+   print("<td>".$row["username"]."</td>\n");
+   print("</tr>\n");
+   print("<tr>\n");
+   print("<td class=\"right\">".$lang['enter_current_password'].":</td>\n");
+   print("<td><input type=\"password\" name=\"old_password\"></td>\n");
+   print("</tr>\n");
+   print("<tr>\n");
+   print("<td class=\"right\">".$lang['enter_new_password'].":</td>\n");
+   print("<td><input type=\"password\" name=\"mod_password\"></td>\n");
+   print("</tr>\n");
+   print("<tr>\n");
+   print("<td class=\"right\">".$lang['confirm_new_password'].":</td>\n");
+   print("<td><input type=\"password\" name=\"mod_confirm\"></td>\n");
+   print("</tr>\n");
+   print("<tr>\n");
+   print("<td colspan=\"2\">&nbsp;</td>\n");
+   print("</tr>\n");
+   print("<tr>\n");
+   print("<td class=\"centered\" colspan=\"2\"><INPUT value=\"".$lang['set_password']."\" class=\"button\" type=\"submit\"></td>\n");
+   print("</tr>\n");
+   print("</form>\n");
+   print("</table>");
+}
+else
+{
+   print("<h2 align=\"center\">".$lang['guests_cant_change_password']."</h2>");
+}
+$help_keyword="password";
+
+require("tail.inc");
+?>
